811499828738981df2129f2452221b5910a4dc2f
xez <xez150140@163.com>
2023-02-07 09:10:54 +0800

    second2

---
+++ src/main/java/com/xez/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xez.git;
+
+/**
+ * @author: xez
+ * @date: 2023/2/1 16:08
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+
+    }
+}
