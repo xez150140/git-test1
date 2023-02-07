824c73a89af108c320efca7ec3f6eb37067fa52d
xez <xez150140@163.com>
2023-02-07 09:40:01 +0800

    master2

--- src/main/java/com/xez/git/GitTest.java
+++ src/main/java/com/xez/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello1");
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix test");
+        System.out.println("master test");
 
 
     }
