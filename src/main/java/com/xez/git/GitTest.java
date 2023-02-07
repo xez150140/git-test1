14367360cb5db0803d3d90d87415c7adefe980e7
xez <xez150140@163.com>
2023-02-07 09:36:39 +0800

    fix1

--- src/main/java/com/xez/git/GitTest.java
+++ src/main/java/com/xez/git/GitTest.java
@@ -8,6 +8,8 @@ package com.xez.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix test");
 
 
     }
